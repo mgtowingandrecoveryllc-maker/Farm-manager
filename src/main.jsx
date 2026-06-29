b56f66f067782032745c826e824bb575c711eeca
mgtowingandrecoveryllc-maker <mgtowingandrecoveryllc@gmail.com>
2026-06-29 13:59:33 -0500

    add src folder

---
+++ src/main.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import FarmManager from "./FarmManager.jsx";
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <FarmManager />
+  </React.StrictMode>
+);
